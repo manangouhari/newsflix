24421963c5131641c9ccf1b98f2b6acdb80e254e
Manan <gouhari21@hotmail.com>
2020-07-06 19:32:53 +0530

    Added loading animation

--- src/components/NewsGrid.js
+++ src/components/NewsGrid.js
@@ -1,24 +1,33 @@
 import React, {useState, useContext} from 'react';
 import NewsItem from './NewsItem';
 import {GlobalContext} from '../context/GlobalContext'
-import {Grid, Fade} from '@material-ui/core'
+import {Grid, Fade, CircularProgress} from '@material-ui/core'
 
 export default () => {
-    const { NewsContext } = useContext(GlobalContext)
+    const { NewsContext, LoadingContext } = useContext(GlobalContext)
     const [news, setNews] = NewsContext;
+    const [loading, setLoading] = LoadingContext;
 
     return(
-        <Fade in={news.length > 0}>
+        
         <Grid container spacing={3} direction="column">
-        { news.map(
+        {loading && 
+        (<Grid item style={{margin: 'auto'}}>
+            <CircularProgress/>
+        </Grid>)
+        }
+        {!loading && news.map(
             newsItem => (
                 <Grid item>
                     <NewsItem key={newsItem.link} {...newsItem}/>
                 </Grid>
             )
-        ) }
+        )}
+        
         
         </Grid>
-        </Fade>
+        
+         
+        
     )
 }
--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -8,12 +8,17 @@ export const GlobalProvider = ({children}) => {
     
     const [news, setNews] = useState([]);
     const [query, setQuery] = useState('');
+    const [loading, setLoading] = useState(true);
+
     const fetchNews = useCallback(_.debounce((q = null) => {
         const URL = `http://127.0.0.1:8000/news${q ? `?query=${q}` : ``}`
         setNews([])
+        setLoading(true)
         axios.get(URL)
-            .then(resp => setNews(_.shuffle(resp.data)))
-
+            .then(resp => {
+                setNews(_.shuffle(resp.data))
+                setLoading(false)
+            })
     }, 500), [query]);
 
     useEffect(fetchNews, [])
@@ -24,6 +29,7 @@ export const GlobalProvider = ({children}) => {
         <GlobalContext.Provider value={{
             NewsContext: [news, setNews],
             QueryContext: [query, setQuery],
+            LoadingContext: [loading, setLoading],
             fetchNewsWithQuery
         }}>
             {children}
